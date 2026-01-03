11aa7f1b1caba85e02c31f5deb3d551610254656
mick811 <mikkelelvers@outlook.com>
2026-01-03 06:59:32 +0100

    add utility plugins (autopairs, surround)

---
+++ nvim/lua/m/plugins/utils.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        opts = {},
+    },
+    {
+        "kylechui/nvim-surround",
+        version = "*",
+        event = "VeryLazy",
+        main = "nvim-surround",
+        opts = {},
+    },
+}
+
