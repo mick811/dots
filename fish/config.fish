696587589ab50250f79c40b6a08065202f4b373c
mick811 <mikkelelvers@outlook.com>
2026-01-03 04:28:06 +0100

    configure fish shell

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,15 @@
 if status is-interactive
 # Commands to run in interactive sessions can go here
 end
+
+# disable fish greeting
+set -g fish_greeting
+
+# initialize oh-my-fish
+if test -f ~/.local/share/omf/init.fish
+    if not set -q OMF_PATH
+        set -gx OMF_PATH ~/.local/share/omf
+    end
+    source $OMF_PATH/lib/require.fish
+    source ~/.local/share/omf/init.fish
+end
